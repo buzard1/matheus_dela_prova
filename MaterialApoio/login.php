08fa7c82a0f919a7200ecce32f05fefcac5131b7
matheus anjos <matheusdlanjos@hotmail.com>
2025-08-08 17:07:59 -0300

    commit

--- MaterialApoio/login.php
+++ MaterialApoio/login.php
@@ -1,60 +1,65 @@
-<?php 
+<?php
 session_start();
 require_once 'conexao.php';
 
-if($_SERVER['REQUEST_METHOD']== "POST"){
+if ($_SERVER['REQUEST_METHOD']== "POST") {
     $email = $_POST['email'];
     $senha = $_POST['senha'];
 
-    $sql = "SELECT * FROM usuario WHERE = :email";
-    $stmt =$pdo->prepare($sql);
-    $stmt->bindParam('email',$email);
+    $sql = "SELECT * FROM usuario WHERE email = :email";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':email', $email);
     $stmt->execute();
     $usuario = $stmt->fetch(PDO::FETCH_ASSOC);
-
-    if($usuario && password_verify($senha,$usuario['senha'])){
-        //LOGIN BEM SUCEDIDO DEFINE VARIAVEIS DE SESSAO
-        $_SESSION['usuario']= $usuario['nome'];
-        $_SESSION['perfil']= $usuario['id_perfil'];
+    if ($usuario && password_verify($senha, $usuario['senha'])){
+        // LOGIN BEM SUCEDIDO DEFINE AS VARIÁVEIS DE SESSÃO
+        $_SESSION['usuario'] = $usuario['nome'];
+        $_SESSION['perfil'] = $usuario['id_perfil'];
         $_SESSION['id_usuario'] = $usuario['id_usuario'];
 
-        //VERIFICA SE A SENHA É TEMPORARIA
-        if($usuario['senha_temporaria']){
-            //REDIRECIONA PARA A TROCA DE SENHA
-            header("Location: alterar_senha.php");
-            exit();
-        }else{
-            //REDIRECIONA PARA A PAGINA PRINCIPAL 
-            header("Location: principal.php");
-            exit();
-        }
+
+        // VERIFICA SE A SENHA É TEMPORÁRIA
+        if ($usuario['senha_temporaria']){
+        // REDIRECIONA PARA A TROCA DE SENHA
+        header("location: alterar_senha.php");
+        exit();
+} else {
+        // REDIRECIONA PARA A PÁGINA PRINCIPAL
+        header("location: principal.php");
+        exit();
+}
     }else{
-        //Login invalido
-        echo "<script>alert('E-mail ou senha incorretos');window.location.href='login.php;</script>";
+        // LOGIN INVALIDO
+        echo "<script>alert('E-mail ou senha inválidos!');window.location.href = 'login.php';
+        </script>";
     }
 
 }
 
 ?>
+
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel = "stylesheet" href = "styles.css">
 </head>
 <body>
+    <h2>Login</h2>
     <form action="login.php" method="POST">
-
-        <label for="email">E-mail</label>
+        <label for="email">E-mail:</label>
         <input type="email" id="email" name="email" required>
-
-        <label for="senha">Senha</label>
+        
+        <label for="senha">Senha:</label>
         <input type="password" id="senha" name="senha" required>
-
+        
         <button type="submit">Entrar</button>
     </form>
-    <p><a href="recuperar_senha.php">Esqueci a minha senha</a></p>
+
+    <p><a href="recuperar_senha.php">Esqueci minha senha</a></p>
 </body>
+<center> <address> Matheus dela libera dos anjos/ Estudante / Tecnico em Deenvolvimento de Sistemas </address> </center>
+
 </html>
