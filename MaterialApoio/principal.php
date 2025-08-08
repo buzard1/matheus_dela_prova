38c7b475314ee1eee51999208f353b041aa3b644
matheus anjos <matheusdlanjos@hotmail.com>
2025-08-08 17:08:11 -0300

    commit

--- MaterialApoio/principal.php
+++ MaterialApoio/principal.php
@@ -25,9 +25,11 @@ $permissoes = [
     "Excluir"=>["excluir_usuario.php","excluir_perfil.php","excluir_cliente.php","excluir_fornecedor.php","excluir_produto.php","excluir_funcionario.php"]],
 
     2 =>["Cadastrar"=>["cadastro_cliente.php"],
-    "Buscar"=>["buscar_usuario.php","buscar_perfil.php","buscar_cliente.php","buscar_fornecedor.php","buscar_produto.php","buscar_funcionario.php"],
-    "Alterar"=>["alterar_usuario.php","alterar_perfil.php","alterar_cliente.php","alterar_fornecedor.php","alterar_produto.php","alterar_funcionario.php"],
-    "Excluir"=>["excluir_usuario.php","excluir_perfil.php","excluir_cliente.php","excluir_fornecedor.php","excluir_produto.php","excluir_funcionario.php"]]
+    "Buscar"=>["buscar_cliente.php","buscar_fornecedor.php","buscar_produto.php"],
+    "Alterar"=>["alterar_fornecedor.php","alterar_produto.php"],
+    "Excluir"=>["excluir_produto.php"]],
+
+    
 ]
 
 ?>
